7a869e50eab2799557d7525a59b1bd58401f817b
Eric Garza <Garza@Erics-MacBook-Pro.local>
2017-11-22 15:03:17 -0800

    Preprocess data and predict test results

--- DataMining.py
+++ DataMining.py
@@ -1,14 +1,27 @@
+from sklearn.svm import LinearSVC
+from sklearn.feature_selection import SelectFromModel
+from sklearn import svm
 import numpy as np
 import sys
 
 def setData( temp, max_coln ):
-	data = np.zeros(shape=(1843, int(max_coln + 1)))
+    data = np.zeros(shape=(1842, int(max_coln + 1)))
 
-	for x in temp:
-		row = int(x[0])
-		col = int(x[1])
-		data[row,col] = x[2]
-	return data
+    for x in temp:
+            row = int(x[0]) - 1
+            col = int(x[1])
+            data[row,col] = x[2]
+    return data
+
+def setTestingData(temp, max_column):
+    data = np.zeros(shape=(952, int(max_column + 1)))
+
+    for x in temp:
+	row = int(x[0]) - 1
+	col = int(x[1])
+	data[row,col] = x[2]
+	
+    return data
 
 def main():
 	training_file = sys.argv[1]
@@ -18,7 +31,23 @@ def main():
 	class_data = np.genfromtxt( classes_file )
 	temp = np.genfromtxt(training_file)
 	data_set = setData( temp, temp.max() )
-	print(data_set)
+        
+        # preprocess data through L1 based feature selection
+        lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(data_set, class_data)
+        model = SelectFromModel(lsvc, prefit=True)
+        data_set_new = model.transform(data_set)
+
+        # preprocess testing data
+        temp_test_data = np.genfromtxt(testing_file)
+        test_data = setTestingData(temp_test_data, temp_test_data.max())
+        test_data_new = model.transform(test_data)
+
+        # create classifier
+        clf = svm.LinearSVC()
+        clf.fit(data_set_new, class_data)
+        results = clf.predict(test_data_new)
+
+        print(results)
 
 
 main()
