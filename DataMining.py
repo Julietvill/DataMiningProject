fccecd84143dbc4f52ce615ffc1a9332734a56b5
Eric Garza <Garza@Erics-MacBook-Pro.local>
2017-12-11 17:22:02 -0800

    Run classifier on test data and write label results to file

--- DataMining.py
+++ DataMining.py
@@ -35,6 +35,18 @@ def setTestingData(temp, max_column):
     return data
 
 
+# extract features selected in feature reduction process and use the same for the test data
+def reduce_test_data(indexes_to_keep, test_data):
+    reduced_test_data = np.zeros(shape=(952, indexes_to_keep.size))
+    for element_index in range(0, 952):
+        feature_count = 0
+        for idx in indexes_to_keep:
+            reduced_test_data[element_index][feature_count] = test_data[element_index][idx]
+            feature_count = feature_count + 1
+
+    return reduced_test_data
+
+
 def getDataSets():
     training_file = 'training.txt'
     training_labels_file = 'label_training.txt'
@@ -52,12 +64,12 @@ def getDataSets():
 def main():
     # get data sets in numpy array to run scikit algorithms on
     data_set, data_labels, test_data = getDataSets() 
-    X_train, y_train, X_test, y_test = train_test_split(data_set, data_labels, test_size=0.2)
 
-    # Reduce features based off feature variance. Line below removes features that are 0 in more than 80% of the features
+    # Reduce features based off feature variance.
     sel = VarianceThreshold(threshold=(.05))
     feature_reduced_data_set = sel.fit_transform(data_set)
-    print(feature_reduced_data_set.shape)
+    indexes_selected = sel.get_support(indices=True)
+    reduced_test_data = reduce_test_data(indexes_selected, test_data)
 
     # train various classifiers
     neigh = KNeighborsClassifier(n_neighbors=50)
@@ -72,6 +84,14 @@ def main():
     print("ANN Accuracy: %0.2f (+/- %0.2f)" % (ann_scores.mean(), ann_scores.std() * 2))
     print("SVM Accuracy Score: %0.2f (+/- %0.2f)" % (svm_scores.mean(), svm_scores.std() * 2))
     
+    # SVM was best algorithm, so label test data through classifer and write to file
+    svm.fit(feature_reduced_data_set, data_labels)
+    label_training_list = svm.predict(reduced_test_data)
+    f = open('label_test.txt', 'w')
+    for label in label_training_list:
+        f.write(str(int(label)))
+        f.write('\n')
+    print('SVM Classifer used! Test labels written to label_test.txt')
 
 if __name__ == '__main__':
     main()
